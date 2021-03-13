53a0ce79eb307cdda4bc9c7a998fc2b8a243945a
Claudio Sanches <contato@claudiosanches.com>
2021-03-13 00:37:21 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit #abort if any command fails
-
-rm -rf build
-docker run --rm --name slate -v $(pwd)/build:/srv/slate/build -v $(pwd)/source:/srv/slate/source slatedocs/slate
